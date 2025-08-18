d90ec2f45fc174ad21ab00bacc66fac776e66156
darshanchipade <dchipade@gmail.com>
2025-08-18 12:17:42 -0700

    Updated for the spring upgrade and java 21

--- src/test/java/com/apple/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/apple/springboot/SpringbootApplicationTests.java
@@ -1,24 +1,14 @@
 package com.apple.springboot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class // Exclude Hibernate JPA auto-configuration
-})
 class SpringbootApplicationTests {
 
     @Test
     void contextLoads() {
         // This test simply ensures that the Spring application context can load successfully.
-        // With the database auto-configuration excluded, it should now pass.
     }
 
 }
